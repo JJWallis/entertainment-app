0f89d073dafaeed893c19d5cf9323bf7ae677039
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-25 19:52:36 +0000

    tailwind testing ip

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
    return (
       <main>
-         <h1>title</h1>
+         <div className="flex">
+            <div>
+               <h1 className="text-red-300 fixed z-10 inset-0 translate-x-[50%]">
+                  title
+               </h1>
+            </div>
+            <div>
+               <h2>title sd</h2>
+            </div>
+         </div>
       </main>
    )
 }
