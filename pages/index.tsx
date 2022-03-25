992b6aa47f3636b926a4a9bfc8d6ac798642091d
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-25 18:19:05 +0000

    tailwind css configured and working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-   return <main className="main"></main>
+   return (
+      <main className="main">
+         <p>hu</p>
+      </main>
+   )
 }
 
 export default Home
