ee68c0c5bc20c7540634c01c8d72cb181fdf1dcd
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-25 17:50:29 +0000

    google code removed

--- pages/cars/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const CarsList = () => {
-   return <h1>Cars list</h1>
-}
-
-export default CarsList
--- pages/hello.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Hello: React.FC = () => {
-   return <p>hello</p>
-}
-
-export default Hello
