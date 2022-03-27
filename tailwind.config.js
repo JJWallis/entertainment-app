76a11dc3773b49304781113b07ef08050de6fe93
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-27 13:54:06 +0100

    pages folder relocated successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
       './src/components/**/*.{js,ts,jsx,tsx}',
    ],
    mode: 'jit',
    // compiles css as write it
-   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+   purge: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
+   ],
    // remove unused css
    theme: {
       extend: {},
