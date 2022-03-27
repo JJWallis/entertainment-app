18349a9e4a26fe6f0b083542e7f068258e02c904
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-27 19:52:20 +0100

    input comp styles done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
       <main>
          <input
             type="text"
-            className="w-[100%] pb-2 pt-2 placeholder:text-slateBlue, text-headingSM"
+            className="w-[100%] border-0 pb-2 pt-2 text-headingSM text-white bg-deepBlue focus:border-b-2 focus:border-slateBlue focus:outline-none"
             placeholder="Search for movies or TV series"
          />
       </main>
