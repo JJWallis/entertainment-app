0cceb11df698306889427a9ffdd039fb6039e7ff
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-27 17:09:49 +0100

    font sizes done

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,18 +29,11 @@ module.exports = {
          outfit: ['Outfit', 'sans-serif'],
       },
       fontSize: {
-         xs: '.75rem',
-         sm: '.875rem',
-         tiny: '.875rem',
-         base: '1rem',
-         lg: '1.125rem',
-         xl: '1.25rem',
-         '2xl': '1.5rem',
-         '3xl': '1.875rem',
-         '4xl': '2.25rem',
-         '5xl': '3rem',
-         '6xl': '4rem',
-         '7xl': '5rem',
+         bodyS: '.8125rem',
+         bodyM: '.9375rem',
+         headingXS: '1.125rem',
+         headingSM: '1.5rem',
+         headingL: '2rem',
       },
       extend: {
          spacing: {
