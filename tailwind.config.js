5fc9ebeacc1a44b5624dbdc3a2db6007c4d46b2e
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-27 16:08:02 +0100

    tailwind config reordered

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,24 @@ module.exports = {
    ],
    // remove unused css
    theme: {
-      extend: {
-         screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
-         },
-         colors: {
-            red: '#FC4747',
-            deepBlue: '#10141e',
-            slateBlue: '#5A698F',
-            darkBlue: '#161d2f',
-            white: '#ffffff',
-         },
+      screens: {
+         sm: '480px',
+         md: '768px',
+         lg: '976px',
+         xl: '1440px',
       },
+      colors: {
+         red: '#FC4747',
+         deepBlue: '#10141e',
+         slateBlue: '#5A698F',
+         darkBlue: '#161d2f',
+         white: '#ffffff',
+      },
+      fontFamily: {
+         sans: ['Graphik', 'sans-serif'],
+         serif: ['Merriweather', 'serif'],
+      },
+      extend: {},
    },
    plugins: [],
 }
