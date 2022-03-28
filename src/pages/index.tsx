0ffe87d4f0333b449fd41b8c2d5de63d5eab587f
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-28 19:40:56 +0100

    global styles updated and search component finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,10 @@ const Home: NextPage = () => {
       <main>
          <input
             type="text"
-            className="w-[100%] border-0 pb-2 pt-2 text-headingSM text-white bg-deepBlue focus:border-b-2 focus:border-slateBlue focus:outline-none"
             placeholder="Search for movies or TV series"
+            className="w-[100%] border-b-[0.5px] border-b-slateBlue border-opacity-0 pb-2 pt-2 text-headingSM text-white bg-deepBlue hover:border-opacity-100 focus:border-opacity-100 transition-border duration-100 ease-in-out"
          />
-         <p className="font-outfit">test</p>
-         <p>test without</p>
+         {/* transition alpha */}
       </main>
    )
 }
