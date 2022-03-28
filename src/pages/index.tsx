9b14ad82062cf046f0d8961f5f7cdebd6b374a9f
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-28 19:53:21 +0100

    search icon ip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import SearchIcon from '../assets/icon-search.svg'
 
 const Home: NextPage = () => {
    return (
       <main>
-         <input
-            type="text"
-            placeholder="Search for movies or TV series"
-            className="outline-none w-[100%] border-b-2 border-b-slateBlue border-opacity-0 pb-2 pt-2 text-headingSM text-white bg-deepBlue hover:border-opacity-100 focus:border-opacity-100 transition-border duration-150 ease-in-out"
-         />
-         {/* transition alpha */}
+         <div className="border-2">
+            <button
+               aria-label="Search desired movie or tv show"
+               className="border-2 grid place-items-center"
+            >
+               <Image src={SearchIcon} alt="" />
+            </button>
+            <input
+               type="text"
+               placeholder="Search for movies or TV series"
+               className="outline-none w-[100%] border-b-2 border-b-slateBlue border-opacity-0 pb-2 pt-2 text-headingSM text-white bg-deepBlue hover:border-opacity-100 focus:border-opacity-100 transition-border duration-150 ease-in-out"
+            />
+         </div>
       </main>
    )
 }
