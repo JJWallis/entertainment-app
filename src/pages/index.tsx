f85476d77d78b594572001565db3032864a4e8ce
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-29 19:27:40 +0100

    film component styles - image import big

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
+import data from '../data.json'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
    return (
       <main>
          <section aria-labelledby={''} className="border-2 border-red">
             <button className='className="block border-2'>
-               {/* bg img */}
+               {/* <Image
+                  src={data[0].thumbnail.regular.small}
+                  alt="lol"
+                  width={'100%'}
+                  height={'100%'}
+               /> */}
             </button>
          </section>
 
