1f0bb0d4a89f830afbfacf786c8d9fc82ae18b7a
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-29 19:51:54 +0100

    bookmark comp styles done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,11 @@ const Home: NextPage = () => {
             {/* </button>  */}
             <button
                aria-label="bookmark dynamic-film"
-               className="block bg-[#000] bg-opacity-50 hover:bg-white"
+               className="grid place-items-center w-6 h-6 rounded-full bg-[#000] bg-opacity-10 hover:bg-white group transition-bg duration-200 ease-in-out"
             >
                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
                   <path
+                     className="group-hover:stroke-deepBlue transition-stroke duration-200 ease-in-out"
                      d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
                      stroke="#FFF"
                      strokeWidth="1.2"
