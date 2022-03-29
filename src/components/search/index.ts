617876d6f4af2d76813ea5fb3617d392b25ba27d
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-29 18:41:53 +0100

    bookmak component comments added

--- src/components/search/index.ts
+++ src/components/search/index.ts
@@ -1,4 +1,3 @@
 export * from './Search'
 export { default } from './Search'
-// import Search from '../Search' // better import
 // https://www.npmjs.com/package/new-component - new component package to auto generate files
