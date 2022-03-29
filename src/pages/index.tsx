e2f2b745437824bcb7215ce1780064ebd2406f3f
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-29 18:25:01 +0100

    Search component populated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,9 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
-import SearchIcon from '../assets/icon-search.svg'
 
 const Home: NextPage = () => {
    return (
       <main>
-         <div className="grid grid-cols-search">
-            <button
-               aria-label="Search desired movie or tv show"
-               className="grid place-items-center"
-            >
-               <Image src={SearchIcon} alt="" />
-            </button>
-            <input
-               type="text"
-               placeholder="Search for movies or TV series"
-               className="outline-none w-[100%] border-b-2 border-b-slateBlue border-opacity-0 pt-2 caret-red text-headingSM text-white bg-deepBlue hover:border-opacity-100 focus:border-opacity-100 transition-border duration-150 ease-in-out"
-            />
-            {/* error styles */}
-         </div>
+         <p>hello world</p>
       </main>
    )
 }
