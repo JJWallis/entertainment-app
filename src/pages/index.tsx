01531af44f6d65cf296a5127f0555cab8e6b09a8
JJWallis <josh.jamesonwallis@gmail.com>
2022-03-29 19:41:31 +0100

    bookmark btn parent style ip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,15 @@ const Home: NextPage = () => {
                   height={'100%'}
                /> */}
             {/* </button>  */}
-            <button aria-label="bookmark dynamic-film" className="block">
+            <button
+               aria-label="bookmark dynamic-film"
+               className="block bg-[#000] bg-opacity-50 hover:bg-white"
+            >
                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
                   <path
                      d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
                      stroke="#FFF"
-                     strokeWidth="1.5"
+                     strokeWidth="1.2"
                      fill="none"
                   />
                </svg>
@@ -28,5 +31,7 @@ const Home: NextPage = () => {
       </main>
    )
 }
+
+// bookmark + film component
 
 export default Home
