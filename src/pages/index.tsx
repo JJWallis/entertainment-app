32d694ca03266009dd2041d5cc0df0a281961c53
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-05 17:51:22 +0100

    play btn added and text too

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import TestImg from '../assets/thumbnails/112/regular/small.jpg'
 import Play from '../assets/icon-play.svg'
 import Image from 'next/image'
 
+// // process.env.PUBLIC_URL => dunamic imgs
+// test with diff img sizes for TestImg
+
 const Home: NextPage = () => {
    return (
       <main>
@@ -12,7 +15,6 @@ const Home: NextPage = () => {
          >
             <button className="relative block">
                <Image
-                  // process.env.PUBLIC_URL
                   src={TestImg}
                   alt={''}
                   width={'100%'}
@@ -38,16 +40,16 @@ const Home: NextPage = () => {
                      />
                   </svg>
                </button>
-               <div className="absolute z-10 inset-0 grid place-items-center bg-deepBlue bg-opacity-50 opacity-100 rounded-lg">
-                  <button>
+               <div className="absolute z-10 inset-0 grid place-items-center bg-deepBlue bg-opacity-60 opacity-100 rounded-lg">
+                  <button className="flex items-center justify-center gap-2 p-1 pr-3 bg-white bg-opacity-20 rounded-3xl">
                      <Image
-                        // process.env.PUBLIC_URL
                         src={Play}
                         alt={''}
                         width={'20px'}
                         height={'20px'}
                         className="rounded-lg"
                      />
+                     <p className="mt-0.5">Play</p>
                   </button>
                </div>
             </button>
