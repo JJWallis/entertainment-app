c1d13fe7a3d6b5fa00b2077c37acb7e78e84b234
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-05 16:16:13 +0100

    movie component styles ip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,32 +9,33 @@ const Home: NextPage = () => {
             aria-labelledby={''}
             className="grid place-items-center h-20 border-2 border-red"
          >
-            <button
-               className="block bg-cover"
-               style={{ backgroundImage: `${TestImg}` }}
-            >
-               {/* <Image
+            <button className="border-2 grid bg-cover bg-center">
+               <Image
                   src={TestImg}
                   // process.env.PUBLIC_URL
                   alt="lol"
                   width={'100%'}
                   height={'100%'}
-               /> */}
+               />
+               <button
+                  aria-label="bookmark dynamic-film"
+                  className="grid place-items-center w-6 h-6 rounded-full bg-[#000] bg-opacity-10 hover:bg-white group transition-bg duration-200 ease-in-out"
+               >
+                  <svg
+                     width="12"
+                     height="14"
+                     xmlns="http://www.w3.org/2000/svg"
+                  >
+                     <path
+                        className="group-hover:stroke-deepBlue transition-stroke duration-200 ease-in-out"
+                        d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+                        stroke="#FFF"
+                        strokeWidth="1.2"
+                        fill="none"
+                     />
+                  </svg>
+               </button>
             </button>
-            {/* <button
-               aria-label="bookmark dynamic-film"
-               className="grid place-items-center w-6 h-6 rounded-full bg-[#000] bg-opacity-10 hover:bg-white group transition-bg duration-200 ease-in-out"
-            >
-               <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                     className="group-hover:stroke-deepBlue transition-stroke duration-200 ease-in-out"
-                     d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
-                     stroke="#FFF"
-                     strokeWidth="1.2"
-                     fill="none"
-                  />
-               </svg>
-            </button> */}
          </section>
       </main>
    )
