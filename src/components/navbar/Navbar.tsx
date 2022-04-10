333f0a25c6f7977e9f7d18a631c3a9043329d78a
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 19:59:27 +0100

    navbar flex ct setup

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const NavBar: React.FC = () => {
    return (
       <div className="pt-11 pb-11 bg-darkBlue w-[100%]">
-         <div></div>
+         <div className="border-2 w-[90%] m-auto flex">
+            <div>{/* logo */}</div>
+            <nav></nav>
+            <div>{/* profile */}</div>
+         </div>
       </div>
    )
 }
