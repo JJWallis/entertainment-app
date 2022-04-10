9f6c65fb613a1e1599cf1f203b06a4c9db322a6c
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 20:03:18 +0100

    logo appears correctly

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Logo from '../../assets/logo.svg'
+import Image from 'next/image'
 
 const NavBar: React.FC = () => {
    return (
       <div className="pt-11 pb-11 bg-darkBlue w-[100%]">
          <div className="border-2 w-[90%] m-auto flex">
-            <div>{/* logo */}</div>
+            <div className="grid place-items-center">
+               <Image src={Logo} alt="" width="35px" height="35px" />
+            </div>
             <nav></nav>
             <div>{/* profile */}</div>
          </div>
