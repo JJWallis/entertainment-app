8e7f525cd9a7d05100b96ead7daadbf360fdcd7e
JJWallis <josh.jamesonwallis@gmail.com>
2022-04-10 19:54:28 +0100

    navbar comps setup

--- src/components/navbar/index.ts
+++
@@ -1,2 +0,0 @@
-// export * from './FileViewer'
-// export { default } from './FileViewer'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
 import type { NextPage } from 'next'
 import Films from '../data.json'
-import Film from '../components/film'
-import { FilmData } from '../types/Film.interface'
 
 const Home: NextPage = () => {
-   const renderFilms = (films: FilmData[]) => {
-      return films.map((film, idx) => <Film key={idx} {...film} />)
-   }
-
-   return (
-      <main className="grid grid-cols-movies gap-8">{renderFilms(Films)}</main>
-   )
+   return <main></main>
 }
 
 export default Home
